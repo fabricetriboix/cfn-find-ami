271c0a3bde29b555b6ea7c9e41413e4f7d893e24
Fabrice Triboix <ftriboix@incise.co>
2019-04-04 21:12:43 +0100

    Finalize first version

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
 cfn-find-ami
 ============
 
-AWS Lambda function to find AMIs, to be used as a custom resource in CloudFormation templates
+AWS Lambda function to find AMIs, to be used as a custom resource in
+CloudFormation templates.
+
+Install the lambda function
+---------------------------
+
+Create an IAM policy using the `find-ami-lambda-policy.json` file.
+Create an IAM role for the lambda function using the above policy.
+
+Then run the following commands in a bash shell:
+
+```bash
+$ go build find-ami.go
+$ zip find-ami.zip find-ami
+$ aws --region YOUR-REGION lambda create-function \
+    --function-name find-ami --memory 128 \
+    --role arn:aws:iam::281582310019:role/find-ami-lambda-role \
+    --runtime go1.x --zip-file fileb://./find-ami.zip \
+    --handler find-ami
+```
+
+"Call" the lambda function from a CloudFormation template
+---------------------------------------------------------
+
+Checkout the `test-cfn-find-ami.yml` for an example.
+
+The following `Properties` can be set in the custom resource:
+ - `ServiceToken`: Set to the ARN of the above lambda function
+ - `Region`: The region you want to search AMIs in; this property is
+   mandatory
+ - `Debug`: Set to `true` to increase verbosity (the logs from the
+   lambda functions are available in the CloudWatch logs)
+ - `Architecture`: The architecture to filter; mostly likely you
+   should set this to `x86_64`
+ - `Name`: Filter on the AMI names; you can use `*` as wildcards;
+   for example, `*bionic*` will search for images with "bionic"
+   anywhere in their names
+ - `OwnerId`: Filter on the owner id
+ - `RootDeviceType`: Filter on the root device type; can be "ebs" or
+   "instance-store"; if not set, this defaults to "ebs"
+ - `VirtualizationType`: Filter on the virtualization type; can be
+   "hvm" or "paravirtual"; if not set, this defaults to "hvm"
+
+The lambda function will filter AMIs that match the parameters you
+provided, and will return the most recent image. The following output
+parameters are available:
+ - `Id`: The AMI id of the found AMI
+ - `Name`: The name of the found AMI
+ - `Description`: The description of the found AMI
--- find-ami.go
+++ find-ami.go
@@ -1,9 +1,11 @@
 package main
 
+
 import (
 	"context"
 	"fmt"
 	"errors"
+	"sort"
 
 	"github.com/aws/aws-lambda-go/cfn"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,6 +15,9 @@ import (
 	"github.com/google/uuid"
 )
 
+
+// Extract a string from the given `event`. Returns the empty string
+// in case of any error.
 func getString(event *cfn.Event, key string) (value string) {
 	value = ""
 	defer func() {
@@ -22,35 +27,52 @@ func getString(event *cfn.Event, key string) (value string) {
 	return
 }
 
+
+// Lambda event handler
 func findAmi(ctx context.Context, event cfn.Event) (physicalId string, data map[string]interface{}, err error) {
-	physicalId = uuid.New().String()  // Random UUID
 	if event.RequestType == "Delete" {
 		fmt.Println("Request type is 'Delete'; nothing to do")
 		return
 	}
 	fmt.Printf("Request type is '%v'\n", event.RequestType)
 
+	// Generate a random physial ID to keep CloudFormation happy
+	physicalId = uuid.New().String()  // Random UUID
+
+	// Extract filter values from the `ResourceProperties`
 	region := getString(&event, "Region")
 	if region == "" {
-		errorString := "'Region' must be set in the request's ResourceProperties"
-		fmt.Println(errorString)
-		err = errors.New(errorString)
+		err = errors.New("'Region' must be set in the request's ResourceProperties")
+		fmt.Println(err)
 		return
 	}
+	debug := getString(&event, "Debug")
 	architecture := getString(&event, "Architecture")
 	name := getString(&event, "Name")
 	ownerId := getString(&event, "OwnerId")
-	fmt.Println("Requested region:", region)
-	fmt.Println("Requested architecture:", architecture)
-	fmt.Println("Requested name:", name)
-	fmt.Println("Requested owner id:", ownerId)
+	rootDeviceType := getString(&event, "RootDeviceType")
+	if rootDeviceType == "" {
+		rootDeviceType = "ebs"
+	}
+	virtualizationType := getString(&event, "VirtualizationType")
+	if virtualizationType == "" {
+		virtualizationType = "hvm"
+	}
+	if debug == "true" {
+		fmt.Println("Requested debug: true")
+		fmt.Println("Requested region:", region)
+		fmt.Println("Requested architecture:", architecture)
+		fmt.Println("Requested name:", name)
+		fmt.Println("Requested owner id:", ownerId)
+		fmt.Println("Requested root device type:", rootDeviceType)
+	}
 
+	// Query the EC2 service to list the matching AMIs
 	cfg := &aws.Config{
 		Region: &region,
 	}
 	sess := session.Must(session.NewSession(cfg))
 	ec2Service := ec2.New(sess)
-
 	input := new(ec2.DescribeImagesInput)
 	if architecture != "" {
 		input.Filters = append(input.Filters, &ec2.Filter{
@@ -70,21 +92,39 @@ func findAmi(ctx context.Context, event cfn.Event) (physicalId string, data map[
 			Values: []*string{&ownerId},
 		})
 	}
-
+	input.Filters = append(input.Filters, &ec2.Filter{
+		Name: aws.String("root-device-type"),
+		Values: []*string{&rootDeviceType},
+	})
+	input.Filters = append(input.Filters, &ec2.Filter{
+		Name: aws.String("virtualization-type"),
+		Values: []*string{&virtualizationType},
+	})
 	output, err := ec2Service.DescribeImages(input)
-	if err == nil {
-		fmt.Println("API call to EC2.DescribeImages() succeeded")
-		fmt.Printf("  %d images found", len(output.Images))
-		fmt.Printf("  output: %+v\n", output)
-		data = map[string]interface{} {
-			"Id": output.Images[0].ImageId,
-			"Name": output.Images[0].Name,
-		}
-	} else {
+	if err != nil {
 		fmt.Printf("API call to EC2.DescribeImages() failed: %v\n", err)
+		return
+	}
+	fmt.Println("API call to EC2.DescribeImages() succeeded")
+	fmt.Printf("%d images found", len(output.Images))
+	if len(output.Images) == 0 {
+		errorString := "No image found for the given filters"
+		err = errors.New(errorString)
+		return
+	}
+
+	// Sort the matching AMIs by creation date and return the latest one
+	sort.Slice(output.Images, func(i, j int) bool {
+		return *(output.Images[i].CreationDate) > *(output.Images[j].CreationDate)
+	})
+	data = map[string]interface{} {
+		"Id": *(output.Images[0].ImageId),
+		"Name": *(output.Images[0].Name),
+		"Description": *(output.Images[0].Description),
 	}
 	return
 }
+
 
 func main() {
 	lambda.Start(cfn.LambdaWrap(findAmi))
